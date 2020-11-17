34aaded6de7eceadc025ce0e95a78d106a0aa805
Mia Parker <mparker24@biomedscienceacademy.org>
2020-11-17 16:02:41 +0000

    I added an f command, while making the final product. I also added \n commands to give the story some space

--- main.py
+++ main.py
@@ -1,7 +1,69 @@
 """
 MadLibs
-Author: 
-Period/Core:
-
-
+Mia Parker
+Core 6
 """
+print("Enter an adjective (description word)")
+#big
+madlib1 = str(input())
+print("Enter a place")
+#disney
+madlib2 = str(input())
+print("Enter an adjective (different from the first) ")
+#short
+madlib3 = str(input())
+print("Enter another different adjective")
+#sour
+madlib4 = str(input())
+print("Enter a plural noun")
+#monkeys
+madlib5 = str(input())
+print("Enter a plural noun (different from the first)")
+#toys
+madlib6 = str(input())
+print("Enter a noun (person, place, thing)")
+#doctor
+madlib7 = str(input())
+print("Enter a  verb (an action word)")
+#jump
+madlib8 = str(input())
+print("Enter a noun (different from the first)")
+#gas station
+madlib9 = str(input())
+print("Enter a verb (different from the first)")
+#run
+madlib10 = str(input())
+print("Enter another action verb")
+#skip
+madlib11 = str(input())
+print("Enter a plural noun")
+#cups
+madlib12 = str(input())
+print("Enter another different noun")
+#candle
+madlib13 = str(input())
+print("Enter a verb ending in ing")
+#sweating
+madlib14 = str(input())
+print("Enter another different noun")
+#joe biden
+madlib15 = str(input())
+print("Enter a measurement of time (Ex. seconds, minutes, etc)")
+#hours
+madlib16 = str(input())
+print("Enter another different adjective")
+#round
+madlib17 = str(input())
+print("Enter another different verb")
+#walk
+madlib18 = str(input())
+print("Enter another different verb")
+#hide
+madlib19 = str(input())
+print("Enter another different adjective")
+#loud
+madlib20 = str(input())
+print("Enter a possesive noun")
+#aunt's
+madlib21 = str(input())
+print(f"Let's put it all together! \n On the {madlib1} trip to {madlib2} my {madlib3} friend and I decided to invent a game.  \n Since this would be a rather {madlib4} trip, it would need to be a game with {madlib5} and {madlib6}. Using our {madlib7} to {madlib8}, we tried to get the {madlib9} next to us to play too, but they just {madlib10} ed at us and {madlib11} away. \n After a few rounds, we thought the game could use some {madlib12} so we turned on the {madlib13} and started {madlib14} to the {madlib15} that came on. This lasted for {madlib16} before I got {madlib17} and decided to {madlib18}. \n I'll never {madlib19} that trip, it was the {madlib20} road trip of my {madlib21}.")
