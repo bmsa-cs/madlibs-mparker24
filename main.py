02fe7efcfc9b521a66cdd93f257a4afcab7c1686
Mia Parker <mparker24@biomedscienceacademy.org>
2020-11-30 17:23:51 +0000

    I changed my input commands to be smaller and easier to understadn while still having the same content. I also made my commands more descriptive instead of madlib1 I now have adjective1, place1, verb1, etc.

--- main.py
+++ main.py
@@ -3,67 +3,46 @@ MadLibs
 Mia Parker
 Core 6
 """
-print("Enter an adjective (description word)")
-#big
-madlib1 = str(input())
-print("Enter a place")
-#disney
-madlib2 = str(input())
-print("Enter an adjective (different from the first) ")
-#short
-madlib3 = str(input())
-print("Enter another different adjective")
-#sour
-madlib4 = str(input())
-print("Enter a plural noun")
-#monkeys
-madlib5 = str(input())
-print("Enter a plural noun (different from the first)")
-#toys
-madlib6 = str(input())
-print("Enter a noun (person, place, thing)")
-#doctor
-madlib7 = str(input())
-print("Enter a  verb (an action word)")
-#jump
-madlib8 = str(input())
-print("Enter a noun (different from the first)")
-#gas station
-madlib9 = str(input())
-print("Enter a verb (different from the first)")
-#run
-madlib10 = str(input())
-print("Enter another action verb")
-#skip
-madlib11 = str(input())
-print("Enter a plural noun")
-#cups
-madlib12 = str(input())
-print("Enter another different noun")
-#candle
-madlib13 = str(input())
-print("Enter a verb ending in ing")
-#sweating
-madlib14 = str(input())
-print("Enter another different noun")
-#joe biden
-madlib15 = str(input())
-print("Enter a measurement of time (Ex. seconds, minutes, etc)")
-#hours
-madlib16 = str(input())
-print("Enter another different adjective")
-#round
-madlib17 = str(input())
-print("Enter another different verb")
-#walk
-madlib18 = str(input())
-print("Enter another different verb")
-#hide
-madlib19 = str(input())
-print("Enter another different adjective")
-#loud
-madlib20 = str(input())
-print("Enter a possesive noun")
-#aunt's
-madlib21 = str(input())
-print(f"Let's put it all together! \n On the {madlib1} trip to {madlib2} my {madlib3} friend and I decided to invent a game.  \n Since this would be a rather {madlib4} trip, it would need to be a game with {madlib5} and {madlib6}. Using our {madlib7} to {madlib8}, we tried to get the {madlib9} next to us to play too, but they just {madlib10} ed at us and {madlib11} away. \n After a few rounds, we thought the game could use some {madlib12} so we turned on the {madlib13} and started {madlib14} to the {madlib15} that came on. This lasted for {madlib16} before I got {madlib17} and decided to {madlib18}. \n I'll never {madlib19} that trip, it was the {madlib20} road trip of my {madlib21}.")
+adjective1 = input("Enter an adjective (description word) ")
+
+place1 = input("Enter a place ")
+
+adjective2 = input("Enter an adjective (different from the first) ")
+
+adjective3 = input("Enter another different adjective ")
+
+noun1 = input("Enter a plural noun ")
+
+noun2 = input("Enter a plural noun (different from the first) ")
+
+noun3 = input("Enter a noun (person, place, thing) ")
+
+verb1 = input("Enter a  verb (an action word) ")
+
+noun4 = input("Enter a noun (different from the first) ")
+
+verb2 = input("Enter a verb (different from the first) ")
+
+verb3 = input("Enter another action verb ")
+
+noun5 = input("Enter a plural noun ")
+
+noun6 = input("Enter another different noun ")
+
+verb4 = input("Enter a verb ending in ing ")
+
+noun7 = input("Enter another different noun ")
+
+time1 = input("Enter a measurement of time (Ex. seconds, minutes, etc) ")
+
+adjective4 = input("Enter another different adjective ")
+
+verb5 = input("Enter another different verb ")
+
+verb6 = input("Enter another different verb ")
+
+adjective5 = input("Enter another different adjective ")
+
+noun8 = input("Enter a possesive noun ")
+
+print(f"Let's put it all together! \n On the {adjective1} trip to {place1} my {adjective2} friend and I decided to invent a game.  \n Since this would be a rather {adjective3} trip, it would need to be a game with {noun1} and {noun2}. Using our {noun3} to {verb1}, we tried to get the {noun4} next to us to play too, but they just {verb2}ed at us and {verb3} away. \n After a few rounds, we thought the game could use some {noun5} so we turned on the {noun6} and started {verb4} to the {noun7} that came on. This lasted for {time1} before I got {adjective4} and decided to {verb5}. \n I'll never {verb6} that trip, it was the {adjective5} road trip of my {noun8}.")
